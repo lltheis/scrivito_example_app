2b6408ecd2299777445384790204dd6022298587
lltheis <theis.laura.l@gmail.com>
2016-09-23 12:08:18 +0000

    editing stylesheets, header & footer

--- app/models/headline_widget.rb
+++ app/models/headline_widget.rb
@@ -3,7 +3,7 @@ class HeadlineWidget < Widget
   attribute :alignment, :enum, values: alignments, default: alignments.first
   attribute :anchor, :string
   attribute :headline, :string
-  attribute :tag, :enum, values: %w[h1 h2 h3 h4 h5 h6], default: "h3"
+  attribute :tag, :enum, values: %w[h1 h2 h3 h4 h5 h6], default: "h1"
 
   def text_extract
     headline
